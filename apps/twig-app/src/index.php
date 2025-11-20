790cbba888c5329b82215555ef751fe519b7c1f4
Trayshmhirk <harlex.mikkey@gmail.com>
2025-11-20 16:14:00 +0100

    feat(twig-app): add source code and templates

---
+++ apps/twig-app/src/index.php
@@ -0,0 +1,34 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader(__DIR__ . '/templates');
+$twig = new \Twig\Environment($loader,[
+    'cache' => __DIR__ . '/../cache/twig',
+    'auto_reload' => true,
+]);
+
+// Simple case based router: Add your routes here
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+
+
+// Add cases for different routes and pass variables to the templates as needed
+switch($path){
+    case '/':
+    case '/home':
+        // Eg: passing a 'title' variable to the landing.twig template
+        echo $twig->render('landing.twig',[
+            'title'=> "Landing Page - Twig Starter Template",
+            
+        ]);
+        break;
+    case '/dashboard':
+        echo $twig->render('dashboard.twig',[
+            'title'=> "Dashboard - Twig Starter Template",
+        ]);
+        break;
+    default:
+    echo $twig->render('404.twig',[
+        'title'=> "404 Not Found - Twig Starter Template",
+    ]);
+};
+?>
